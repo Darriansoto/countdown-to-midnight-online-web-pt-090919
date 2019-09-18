50cc1f4893611cdeadd40f6eba79e1a62b858c3e
Darrian Soto <ddog350@gmail.com>
2019-09-18 04:21:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,8 @@ def countdown
   while x > 0 
     puts "#{x} SECOND(S)!"
     x -= 1
-    if x == 0 
-      puts "HAPPY NEW YEAR!"
-    end
+  end
+  if x == 0 
+    puts "HAPPY NEW YEAR!"
   end
 end
